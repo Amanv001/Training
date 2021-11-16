90980c372b0fa22fcc4c2ecefc0c6717c0769bd2
Amanv001 <amankumar@benchmarkit.solutions>
2021-11-16 16:13:07 +0530

    Challenge4

---
+++ js/Coding Challenge/Challenge4/script.js
@@ -0,0 +1,5 @@
+//                          Challenge4
+let bill = 275;
+let tip = (bill>50 || bill<300) ? bill * (15/100) : bill * (20/100);
+let total = bill + tip;
+console.log("The bill was "+bill+", the tip was "+tip+" and the total value "+total);
