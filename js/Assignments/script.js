a2c7627e346ae5e654b71bed837ac328fb41ae2b
Amanv001 <amankumar@benchmarkit.solutions>
2021-11-17 11:03:48 +0530

    FP1 Done

--- js/Assignments/script.js
+++ js/Assignments/script.js
@@ -19,7 +19,7 @@ console.log(typeof(isIsland));
 
 //                                Lecture: let,const,Variables
 
-const language = "Hindi"
+const language = "hindi"
 const country = "India"
 const continent = "Asia"
 
@@ -50,3 +50,91 @@ else {
 }
 
 let description = "Portugal is in Europe, and its 11 million people speak portuguese"
+
+//                                  Lecture: Strings and Template Literals
+description = `Portugal is in Europe,
+and its 11 million people
+speak portuguese`
+console.log(description);
+
+//                              Lecture:Taking Decisions: if / else Statements
+if (population > 33000000) {
+  console.log(`India's population is above average.`);
+}
+else {
+  console.log(`India's population is $(Population-33) million below average.`);
+}
+population = 13;
+population = 130;
+
+//                            LECTURE: Type Conversion and Coercion
+console.log(`9` - `5`);
+console.log(`9` - `13` + `17`);
+console.log(`9` - `13` + 17);
+console.log(`123` < 57);
+console.log(5+6+`4`+9-4-2);
+
+//                            LECTURE: Equality Operators: == vs. ===
+// let numNeighbours = prompt("How many neighbour countries does your country have?");
+// numNeighbours = Number(numNeighbours);
+// if (numNeighbours == 1) {
+//   console.log("Only 1 border");
+// }
+// else if(numNeighbours > 1){
+//   console.log("More than 1 border");
+// }
+// else {
+//   console.log("No borders");
+// }
+
+// if (numNeighbours === 1) {
+//   console.log("Only 1 border");
+// }
+// else if(numNeighbours > 1){
+//   console.log("More than 1 border");
+// }
+// else {
+//   console.log("No borders");
+// }
+
+//                                LECTURE: Logical Operators
+if (language === "English") {
+  if (population < 5000000) {
+    if (isIsland == false) {
+      console.log(`You should live in India`);
+    }
+    else {
+      console.log(`India doesn't meet your criteria`);
+    }
+  }
+  else {
+    console.log(`India doesn't meet your criteria`);
+  }
+}
+else {
+  console.log(`India doesn't meet your criteria`);
+}
+
+//                            LECTURE: The switch Statement
+switch (language) {
+  case 'chinese || mandarin':
+    console.log(`Most number of native speakers`);
+    break;
+  case 'spanish':
+    console.log(`2nd place in number of native speakers`);
+    break;
+  case 'english':
+    console.log(`3rd place`);
+    break;
+  case 'hindi':
+    console.log(`Number 4`);
+    break;
+  case 'arabic':
+    console.log(`5th most spoken language`);
+    break;
+  default:
+    console.log(`Great lnguage too :D`);
+}
+
+//                                LECTURE: The Conditional (Ternary) Operator
+console.log(population>3000000 ? `India's population is above average` : `India's population is below average`);
