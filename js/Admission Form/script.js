5d7cd3dcb955e27194c3b00f6e292df78b72f815
Amanv001 <amankumar@benchmarkit.solutions>
2021-11-21 13:00:53 +0530

    Assignmentdone

---
+++ js/Admission Form/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+$(document).ready(function () {
+    $('[data-toogle = "tooltip"]').tooltip();
+
+    var checkbox = $('table tbody input[type="checkbox"]');
+    $('#selectAll').click(function () {
+      if (this.checked) {
+        checkbox.each(function () {
+          this.checked = true;
+        });
+      } else {
+        checkbox.each(function () {
+          this.checked = false;
+        });
+      }
+    });
+    checkbox.click(function () {
+      if (!this.checked) {
+        $('#selectAll').prop("checked", false);
+      }
+    });
+  });
