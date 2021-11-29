1a0e7101d25582734ac03a9a20892ea1fba4167c
Amanv001 <amankumar@benchmarkit.solutions>
2021-11-29 21:28:37 +0530

    jsDone

--- Admission Form/script.js
+++ Admission Form/script.js
@@ -1,99 +1,139 @@
-'use strict';
+getTableData();
 
-$(document).ready(function () {
-  $('[data-toogle = "tooltip"]').tooltip();
+function getTableData() {
+    var members = getMembers();
+    if (members.length == 0) {
+        document.querySelector('.show-table-info').classList.remove('hide');
+    }
+    else {
+        members.forEach(function (item, index) {
+            insertIntoTableView(item, index + 1);
+        });
+    }
+}
 
-  var checkbox = $('table tbody input[type="checkbox"]');
-  $('#selectAll').click(function () {
-    if (this.checked) {
-      checkbox.each(function () {
-        this.checked = true;
-      });
+function getMembers() {
+    var memberRecord = localStorage.getItem("members");
+    var members = [];
+    if (!memberRecord) {
+        return members;
     } else {
-      checkbox.each(function () {
-        this.checked = false;
-      });
+        members = JSON.parse(memberRecord);
+        return members;
     }
-  });
-  checkbox.click(function () {
-    if (!this.checked) {
-      $('#selectAll').prop("checked", false);
+}
+
+function insertIntoTableView(item, tableIndex) {
+    var table = document.getElementById('student_table');
+    var row = table.insertRow();
+    var idCell = row.insertCell(0); // id
+    var nameCell = row.insertCell(1); // name
+    var emailCell = row.insertCell(2); // description
+    var phoneCell = row.insertCell(3); // name
+    var placeCell = row.insertCell(4); // email
+    var actionCell = row.insertCell(5);
+
+    var guid = item.id;
+    idCell.innerHTML = tableIndex;
+    nameCell.innerHTML = item.name;
+    emailCell.innerHTML = item.email;
+    phoneCell.innerHTML = item.phone;
+    placeCell.innerHTML = item.place;
+    actionCell.innerHTML = `<button class="btn btn-sm btn-primary" onclick="showEditModal(${guid})"><i class="fa fa-pencil" aria-hidden="true"></i></button>` + `&nbsp;<button class="btn btn-sm btn-danger" onclick="showDeleteModal(${guid})"><i class="fa fa-trash" aria-hidden="true"></i></button>`;
+}
+
+
+// Unique ID for each record
+function guid() {
+    return parseInt(Date.now() + Math.random());
+}
+
+// Create and Store New Member
+function saveStudentInfo() {
+    var keys = ['name', 'email', 'phone', 'place']; // input ids
+    var obj = {};
+
+    keys.forEach(function (item) {
+        var result = document.getElementById(item).value;
+        if (result) {
+            obj[item] = result;
+        }
+    });
+
+    var members = getMembers();
+
+    if (Object.keys(obj).length) {
+        obj.id = guid();
+        members.push(obj);
+        var data = JSON.stringify(members);
+        localStorage.setItem("members", data);
+        // insertIntoTableView(obj, getTotalRowOfTable());
+        window.location.reload();
+        // document.querySelector('.show-table-info').classList.toggle('hide');
     }
-  });
-});
+}
+
+function getTotalRowOfTable() {
+    var table = document.getElementById('student_table');
+    return table.rows.length;
+}
+
+// Show Edit Modal of a single member
+function showEditModal(id) {
+    var allMembers = getMembers();
+    var member = allMembers.find(function (item) {
+        return item.id == id;
+    });
+    $('#edit_name').val(member.name);
+    $('#edit_email').val(member.email);
+    $('#edit_phone').val(member.phone);
+    $('#edit_place').val(member.place);
+    $('#member_id').val(id);
+
+    $('#editModal').modal();
+}
+
+// Store Updated Member Data into the storage
+function updateStudentData() {
+    var allMembers = getMembers();
+    var memberId = $('#member_id').val();
+
+    var member = allMembers.find(function (item) {
+        return item.id == memberId;
+    })
+
+    member.name = $('#edit_name').val();
+    member.email = $('#edit_email').val();
+    member.phone = $('#edit_phone').val();
+    member.place = $('#edit_place').val();
+
+    var data = JSON.stringify(allMembers);
+    localStorage.setItem('members', data);
+    $('#editModal').modal('hide')
+    window.location.reload();
+}
 
 
+function showDeleteModal(id) {
+    $('#deleted-member-id').val(id);
+    $('#deleteDialog').modal();
+}
 
 
-// var selectedRow = null;
-// function onFormSubmit(e) {
-//   event.preventDefault();
-//   var formData = readFormData();
-//   if (selectedRow === null) {
-//     insertNewRecord(formData);
-//   }
-//   else {
-//     updateRecord(formData);
-//   }
-//   resetForm();
-// }
+function deleteStudentData() {
+    var id = $('#deleted-member-id').val();
 
-// //Retrieve the data
-// function readFormData() {
-//   var formData = {};
-//   formData["name"] = document.getElementById("InputName").value;
-//   formData["email"] = document.getElementById("InputEmail").value;
-//   formData["add"] = document.getElementById("InputAddress").value;
-//   formData["phone"] = document.getElementById("InputPhone").value;
-//   return formData;
-// }
+    var storageUsers = JSON.parse(localStorage.getItem('members'));
 
-// //Insert the data
-// function insertNewRecord(data) {
-//   var table = document.getElementById("studentList").getElementsByTagName('tbody')[0];
-//   var newRow = table.insertRow(table.length);
-//   var cell1 = newRow.insertCell(0);
-//   cell1.innerHTML = data.name;
-//   var cell2 = newRow.insertCell(1);
-//   cell2.innerHTML = data.email;
-//   var cell3 = newRow.insertCell(2);
-//   cell3.innerHTML = data.add;
-//   var cell4 = newRow.insertCell(3);
-//   cell4.innerHTML = data.phone;
-//   var cell5 = newRow.insertCell(4);
-//   cell5.innerHTML = `<a href="#editStudentModal" class="edit" data-toggle="modal"><i class="material-icons"
-//   data-toggle="tooltip" title="Edit">&#xE254;</i></a> <a href="#deleteStudentModal" class="delete" data-toggle="modal"><i class="material-icons"
-//   data-toggle="tooltip" title="Delete">&#xE872;</i></a>`
-// }
+    var newData = [];
 
-// //Edit the data
-// function onEdit(td){
-//     selectedRow = td.parentElement.parentElement;
-//     document.getElementById('productCode').value = selectedRow.cells[0].innerHTML;
-//     document.getElementById('product').value = selectedRow.cells[1].innerHTML;
-//     document.getElementById('qty').value = selectedRow.cells[2].innerHTML;
-//     document.getElementById('perPrice').value = selectedRow.cells[3].innerHTML;
-// }
+    newData = storageUsers.filter(function (item, index) {
+        return item.id != id;
+    });
 
-// function updateRecord(formData){
-//     selectedRow.cells[0].innerHTML = formData.productCode;
-//     selectedRow.cells[1].innerHTML = formData.product;
-//     selectedRow.cells[2].innerHTML = formData.qty;
-//     selectedRow.cells[3].innerHTML = formData.perPrice;
-// }
+    var data = JSON.stringify(newData);
 
-// //Delete the data
-// function onDelete(td){
-//     if(confirm('Do you want to delete this record?')){
-//         row = td.parentElement.parentElement;
-//         document.getElementById('storeList').deleteRow(row.rowIndex);
-//     }
-//     resetForm();
-// }
-//Reset the data
-// function resetForm() {
-//   document.getElementById('productCode').value = '';
-//   document.getElementById('product').value = '';
-//   document.getElementById('qty').value = '';
-//   document.getElementById('perPrice').value = '';
-// }
+    localStorage.setItem('members', data);
+    $('#deleteDialog').modal('hide');
+    window.location.reload();
+}
