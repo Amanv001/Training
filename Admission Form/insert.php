ed3f69934d8aa95759a082b403f3585add841368
Amanv001 <amankumar@benchmarkit.solutions>
2021-12-13 14:01:43 +0530

    Update insert.php

--- Admission Form/insert.php
+++ Admission Form/insert.php
@@ -8,12 +8,30 @@
         $phone = $_POST["phone"];
         $place = $_POST["place"];
 
-        $sql = "INSERT INTO student_info(name,email,phone,place)
-                values('$name','$email','$phone','$place')";
-        if (mysqli_query($conn,$sql)) {
-            echo "<script> alert('Student Added 🧑‍🎓')</script>";
-            // echo "Student Addded";
+        if (!empty($name)) {
+            if (!empty($email)) {
+                if (!empty($phone)) {
+                    if (!empty($place)) {
+                        $sql = "INSERT INTO student_info(name,email,phone,place)
+                                    values('$name','$email','$phone','$place')";
+                        if (mysqli_query($conn,$sql)) {
+                            echo "<script> alert('Student Added 🧑‍🎓')</script>";
+                            // echo "Student Addded";
+                        }
+                    }else {
+                        echo "<script> alert('place cannot be empty')</script>";
+                    }
+                }else {
+                    "<script>alert('phone number cannot be empty')</script>";
+                }
+            }else {
+                echo "<script> alert('Email cannot be empty')</script>";
+            }
+        }else {
+            echo "<script> alert('Name Cannot be Empty!')</script>";
         }
+
+        
     }
 
     header('Location:index.php');
